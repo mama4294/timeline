16d8197c7975d1fefad0cdeeef24ca586d4e5c39
mama4294 <mmalone4294@gmail.com>
2025-09-03 08:59:01 -0600

    feat: connects to Dataverse

---
+++ operations-timeline/.power/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,22 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "cr2b6_batcheses": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr2b6_batchesid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr2b6_equipments": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr2b6_equipmentid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
